68b29c2767b8f223b3b767e2307306de663f3b59
Jack Babb <jwababb@gmail.com>
2019-04-14 01:02:46 -0500

    Add some unit tests and pass command line arguments

--- src/base64/Encoder.rs
+++ src/base64/Encoder.rs
@@ -4,16 +4,15 @@ pub struct Encoder {
 impl Encoder {
     pub fn encode(&self, input: &str) -> String {
         // create output string to hold adjusted characters
-        let mut buf = String::with_capacity(input.len());
+        let mut buf = String::with_capacity(input.len() * (4/3));
         let mut chars = input.chars();
-        let mut stop = false;
-        let mut ch = 'a';
-        let mut bits_to_consume = 6;
-        let mut bits_to_mask = 2;
-        let mut mask = 0;
-        let mut swap = 0;
-        let mut rust = 0;
-        while !stop {
+        let mut ch: char = 'a';
+        let mut bits_to_consume: u8 = 6;
+        let mut bits_to_mask: u8 = 2;
+        let mut mask: u16 = 0;
+        let mut swap: u8 = 0;
+        let mut rust: u8 = 0;
+        loop {
             match chars.next() {
                 Some(c) => ch = c,
                 None => {
@@ -29,16 +28,19 @@ impl Encoder {
                     break;
                 },
             }
-            // println!("BEGIN: {:?}", (mask, swap, rust, bits_to_consume, bits_to_mask));
-            swap = (ch as u8) >> (((ch.len_utf8() as u8) * 8) - bits_to_consume);
+            if bits_to_consume == 0 {
+                swap = 0;
+            } else {
+                swap = (ch as u8) >> (((ch.len_utf8() as u8) * 8) - bits_to_consume);
+            }
             buf.push(self.get_b64_char(rust | swap));
 
             if bits_to_consume == 0 { bits_to_consume = 6; }
             else { bits_to_consume -= 2; }
-            mask = (2 as u8).pow(bits_to_mask as u32) - 1;
-            rust = ((ch as u8) & mask) << bits_to_consume; // bring back our dropped bits 
+            mask = (2 as u16).pow(bits_to_mask as u32) - 1;
+            rust = ((ch as u8) & mask as u8) << bits_to_consume; // bring back our dropped bits 
             bits_to_mask = 8 - bits_to_consume;
-            // println!("  END: {:?}", (mask, swap, rust, bits_to_consume, bits_to_mask));
+            println!("{:?}", (mask, swap, rust, bits_to_consume, bits_to_mask));
         }
         buf
     }
@@ -60,4 +62,29 @@ impl Encoder {
             '+'
         }
     }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn wiki_1() {
+        let enc = Encoder{};
+        assert_eq!(enc.encode("Man"), "TWFu");
+    }
+    #[test]
+    fn wiki_2() {
+        let enc = Encoder{};
+        assert_eq!(enc.encode("Ma"), "TWE=");
+    }
+    #[test]
+    fn wiki_3() {
+        let enc = Encoder{};
+        assert_eq!(enc.encode("M"), "TQ==");
+    }
+    #[test]
+    fn long_string() {
+        let enc = Encoder{};
+        assert_eq!(enc.encode("asdflkjwefoinvvofoifjdfjasdlvk"), "YXNkZmxrandlZm9pbnZ2b2ZvaWZqZGZqYXNkbHZr");
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
+use std::env;
 mod base64;
 use base64::Encoder::Encoder;
 fn main() {
-    let mut to_encode = "Man";
-    let mut expected_result = "TWFu";
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let to_encode = &args[1];
+    let expected_result = &args[2];
     let b64_encoder = Encoder{};
-    println!("{}, {} = {}", to_encode, b64_encoder.encode(to_encode), expected_result);
-    
-    to_encode = "Ma";
-    expected_result = "TWE=";
-    println!("{}, {} = {}", to_encode, b64_encoder.encode(to_encode), expected_result);
-
-    to_encode = "M";
-    expected_result = "TQ==";
     println!("{}, {} = {}", to_encode, b64_encoder.encode(to_encode), expected_result);
 }
 
